a903358bbbac8b63d56c060cd4515e10cf5c86c0
Naptile .M. Peter <naptilepeterson71@gmail.com>
2026-05-16 19:33:49 +0300

    Update server.js

--- auth-backed/server.js
+++ auth-backed/server.js
@@ -21,4 +21,4 @@ app.get("/",(req,res)=>{
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>console.log(`server running on http://localhost:${PORT}`));
+app.listen(PORT,()=>console.log(`https://new-notes-app-i7oo.onrender.com:${PORT}`));
